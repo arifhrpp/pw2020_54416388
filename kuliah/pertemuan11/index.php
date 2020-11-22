95f3fb7e431e8d0461f17f4a87d1eec1655674e6
Arif harahap <arepharahap49@gmail.com>
2020-11-22 19:36:02 +0700

    menambahkan fungsi hapus, ubah dan pencarian

--- kuliah/pertemuan11/index.php
+++ kuliah/pertemuan11/index.php
@@ -2,6 +2,12 @@
 require 'functions.php';
 $mahasiswa = query("SELECT * FROM mahasiswa");
 
+
+// ketika tombol cari diklik
+if (isset($_POST['cari'])) {
+  $mahasiswa = cari($_POST['keyword']);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +24,12 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
   <a href="tambah.php">Tambah Data Mahasiswa</a>
   <br><br>
 
+  <form action="" method="POST">
+    <input type="text" name="keyword" size="30" placeholder="Masukkan keyword pencarian..." autocomplete="off" autofocus>
+    <button type="submit" name="cari">Cari! </button>
+  </form>
+  <br>
+
 
   <table border="1" cellpadding="10" cellspacing="0">
     <tr>
@@ -26,6 +38,14 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
       <th>Nama</th>
       <th>Aksi</th>
     </tr>
+
+    <?php if (empty($mahasiswa)) : ?>
+      <tr>
+        <td colspan="4">
+          <p style="color : red; font-style : italic;">data mahasiswa tidak ditemukan!</p>
+        </td>
+      </tr>
+    <?php endif; ?>
 
     <?php $i = 1;
     foreach ($mahasiswa as $m) :   ?>
